59a83814971142bab7cffaf4a9d4f5bb27eba754
LEOJI <122586785+KIMJIHYEON1994@users.noreply.github.com>
2023-03-14 16:43:22 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int result = 0;
+        if (str1.contains(str2)) {
+            result = 1;
+        } else {
+            result = 2;
+        }
+        return result;
+    }
+}
