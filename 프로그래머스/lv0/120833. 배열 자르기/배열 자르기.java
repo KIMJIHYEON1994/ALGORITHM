d6ff524c9a6d797bcd326deafa30d729c7b80cbe
LEOJI <122586785+KIMJIHYEON1994@users.noreply.github.com>
2023-03-16 15:25:07 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] result = java.util.Arrays.copyOfRange(numbers, num1, num2 + 1);
-
-        return result;
+        return java.util.Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
