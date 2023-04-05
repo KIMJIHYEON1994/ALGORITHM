e8df64317abc368da8b2042794601859b5b5484a
LEOJI <122586785+KIMJIHYEON1994@users.noreply.github.com>
2023-04-05 21:35:44 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.36 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,12 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
+        int len = numbers.length;
 
         Arrays.sort(numbers);
 
         int result1 = numbers[0] * numbers[1];
-        int result2 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        int result2 = numbers[len - 1] * numbers[len - 2];
 
-        return (result1 > result2) ? result1 : result2;
+        return Math.max(result1, result2);
     }
 }
