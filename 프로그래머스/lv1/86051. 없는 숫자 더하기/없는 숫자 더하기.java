0cd08b3ff96f447e759cee9aff114cba6cfa43c4
LEOJI <122586785+KIMJIHYEON1994@users.noreply.github.com>
2023-04-07 10:34:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int total = 0;
+        int sum = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            total += i;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        answer = total - sum;
+        return answer;
+    }
+}
