bb358356adbdecd585283ee39a0c86a35f635a9e
LEOJI <122586785+KIMJIHYEON1994@users.noreply.github.com>
2023-04-08 10:58:30 +0900

    [level 1] Title: 최소직사각형, Time: 1.35 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+
+            if (maxRow < sizes[i][0]) {
+                maxRow = sizes[i][0];
+            }
+
+            if (maxCol < sizes[i][1]) {
+                maxCol = sizes[i][1];
+            }
+        }
+        answer = maxRow * maxCol;
+        return answer;
+    }
+}
