1edc959d08c3a280b8ed1dd0d4b7ba47822f7709
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2023-05-04 16:31:18 +0900

    [Silver IV] Title: 스택, Time: 440 ms, Memory: 21396 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inputCount = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (String inputLine : br.lines().limit(inputCount).toArray(String[]::new)) {
+            String[] input = inputLine.split(" ");
+
+            switch (input[0]) {
+                case "push":
+                    stack.push(Integer.parseInt(input[1]));
+                    break;
+                case "pop":
+                    int stackPop = stack.isEmpty() ? -1 : stack.pop();
+                    System.out.println(stackPop);
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    int stackIsEmpty = stack.isEmpty() ? 1 : 0;
+                    System.out.println(stackIsEmpty);
+                    break;
+                case "top":
+                    int stackTop = stack.isEmpty() ? -1 : stack.peek();
+                    System.out.println(stackTop);
+                    break;
+            }
+
+        }
+
+    }
+}
