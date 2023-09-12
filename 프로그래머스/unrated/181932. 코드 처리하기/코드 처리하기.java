b77648d24daabeb7c1df1bf30d0ae7f52f1bf5fb
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2023-09-12 16:02:14 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String code) {
+        int mode = 0;
+        String answer = "";
+
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                mode = mode == 0 ? 1 : 0;
+            }
+            
+            if (code.charAt(i) == '1') {
+                continue;
+            }
+            
+            if (mode == 0 && i % 2 == 0) {
+                answer += code.charAt(i);
+            }
+            
+            if (mode == 1 && i % 2 != 0) {
+                answer += code.charAt(i);
+            }
+        }
+
+        if (answer.isEmpty()) {
+            answer = "EMPTY";
+        }
+        return answer;
+    }
+}
