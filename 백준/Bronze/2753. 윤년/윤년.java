5bcd361de029d3a6e69ce2b880279b7e33c13d7d
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2024-05-22 10:10:33 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a % 4 == 0) {
+            if (a % 100 != 0 || a % 400 == 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        } else {
+            System.out.println("0");
+        }
+    }
+}
