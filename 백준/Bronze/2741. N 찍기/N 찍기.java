7674cdd6f3b044aea282043baa93a192872fa88c
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2024-05-22 10:25:25 +0900

    [Bronze V] Title: N 찍기, Time: 960 ms, Memory: 30460 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,11 +1,16 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int n = Integer.parseInt(st.nextToken());
 
         for (int i = 1; i <= n; i++) {
             System.out.println(i);
