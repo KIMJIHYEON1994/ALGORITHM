72eb6e2184eb95242f02381d2b5553476afb2ae5
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2024-05-23 17:37:15 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String str = br.readLine();
+            int a = str.charAt(0) - 48;
+            int b = str.charAt(2) - 48;
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+            sb.append((a + b)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
