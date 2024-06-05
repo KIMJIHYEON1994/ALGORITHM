29dcd251faf1fbe05d638dce717d40cc79171843
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2024-06-05 18:37:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 492 ms, Memory: 51044 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -4,21 +4,13 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class Main {
-    static class Person {
-        int age;
-        String name;
-
-        public Person(int age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
 
-        ArrayList<Person>[] ageGroups = new ArrayList[201];
+        ArrayList<String>[] ageGroups = new ArrayList[201];
+        
         for (int i = 0; i <= 200; i++) {
             ageGroups[i] = new ArrayList<>();
         }
@@ -28,13 +20,14 @@ public class Main {
             int age = Integer.parseInt(input[0]);
             String name = input[1];
 
-            ageGroups[age].add(new Person(age, name));
+            ageGroups[age].add(name);
         }
 
         StringBuilder sb = new StringBuilder();
+        
         for (int age = 1; age <= 200; age++) {
-            for (Person p : ageGroups[age]) {
-                sb.append(p.age).append(" ").append(p.name).append("\n");
+            for (String name : ageGroups[age]) {
+                sb.append(age).append(" ").append(name).append("\n");
             }
         }
 
