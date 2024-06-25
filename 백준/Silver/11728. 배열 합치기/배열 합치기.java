183d15cb2c864b4f889aec725cf535e73757fed7
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2024-06-25 21:51:46 +0900

    [Silver V] Title: 배열 합치기, Time: 1256 ms, Memory: 309204 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = new int[n + m];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+
+        while (i < n) {
+            result[k++] = arr1[i++];
+        }
+
+        while (j < m) {
+            result[k++] = arr2[j++];
+        }
+
+        for (int num : result) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
